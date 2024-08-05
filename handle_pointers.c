b776cc8389a90aa75d273312dbdd5b154c62afa9
IZERE <blaiseinnocenti@gmail.com>
2024-08-04 23:22:01 -0700

    done

--- handle_pointers.c
+++ handle_pointers.c
@@ -13,27 +13,27 @@
  *
  * Return: The number of characters added to the string buffer.
  */
-int handle_pointer(__attribute__((unused)) const format_specifier *spec,
-                   va_list args, string_buffer *buffer)
+	int handle_pointer(__attribute__((unused)) const format_specifier * spec,
+			va_list args, string_buffer *buffer)
 {
-    char hex_str[17];
-    int characters_printed;
-    size_t initial_length = buffer->length;
-    unsigned long int pointer = va_arg(args, unsigned long int);
+	char hex_str[17];
+	int characters_printed;
+	size_t initial_length = buffer->length;
+	unsigned long int pointer = va_arg(args, unsigned long int);
 
-    /* Handle NULL pointers by appending "(nil)" to the buffer */
-    if (!pointer)
-    {
-        append_string(buffer, "(nil)");
-    }
-    else
-    {
-        /* Convert the pointer address to a hexadecimal string */
-        utob(pointer, hex_str, HEX);
-        append_string(buffer, "0x"); /* Prefix the hexadecimal string with "0x" */
-        append_string(buffer, hex_str);
-    }
-
-    characters_printed = buffer->length - initial_length;
-    return (characters_printed);
+	/* Handle NULL pointers by appending (nil) to the buffer */
+	if (!pointer)
+	{
+		append_string(buffer, "(nil)");
+		else
+		{
+			/* Convert the pointer address to a hexadecimal string */
+			utob(pointer, hex_str, HEX);
+			append_string(buffer, "0x");
+			/* Prefix the hexadecimal string with (0x) */
+			append_string(buffer, hex_str);
+		}
+		characters_printed = buffer->length - initial_length;
+		return (characters_printed);
+	}
 }
